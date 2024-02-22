86a7606c097d9c7a8fb5f3a249012685f4fec424
jhuseyn <huseyn.jabbarov@proton.me>
2024-02-22 17:29:31 +0400

    fixes, lombok

--- src/main/java/com/hj/VTS/model/Owner.java
+++ src/main/java/com/hj/VTS/model/Owner.java
@@ -1,9 +1,17 @@
 package com.hj.VTS.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Owner {
     @Id
--- src/main/java/com/hj/VTS/model/Project.java
+++ src/main/java/com/hj/VTS/model/Project.java
@@ -1,20 +1,31 @@
 package com.hj.VTS.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
-import java.util.Set;
+import java.time.LocalDateTime;
 
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
     // Standard getters and setters
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
-    @OneToMany(mappedBy = "project")
-    private Set<Vulnerability> vulnerabilities;
+
+
 }
